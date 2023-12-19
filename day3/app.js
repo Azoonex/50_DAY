34848be10dfc31b67a3fd5adaac13826a13584e1
amirabas <abasaarafi@outlook.com>
2023-12-19 13:27:49 -0800

    return noting day3

---
+++ day3/app.js
@@ -0,0 +1,15 @@
+var circle = document.querySelector('.circle');
+var openElment = document.querySelector('#open');
+var closeElment = document.querySelector('#close');
+var open = false;
+circle.addEventListener('click', function () {
+    console.log('return');
+    if (oepn === false) {
+        openElment.classList.add('open');
+    }
+    esle;
+    {
+        closeElment.
+        ;
+    }
+});
---
+++ day3/app.ts
@@ -0,0 +1,14 @@
+let circle = document.querySelector('.circle')!
+let openElment = document.querySelector('#open')!
+let closeElment = document.querySelector('#close')!
+
+let open:boolean = false
+
+circle.addEventListener('click',()=>{
+    console.log('return');
+    if(oepn === false){
+        openElment.classList.add('open')
+    }esle{
+        closeElment.
+    }
+})
