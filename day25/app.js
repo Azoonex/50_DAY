d5a48ae6585e29b63f9eeafec6e01e67c24db18f
amirabas <abasaarafi@outlook.com>
2024-03-25 18:52:01 +0330

    nice im create slider img thanke you

--- day25/app.js
+++ day25/app.js
@@ -20,58 +20,52 @@ console.log(xs.length);
 var x = 4;
 console.log(x.length); // prints undefined since numbers don't have a length
 // casting with <>
-console.log(xs.length);
-var value = null;
-value = 'hellow';
-wipe;
-he;
-wiped;
-his;
-hands;
-on;
-a;
-clean;
-towel,
-    weigh;
-weigh;
-weigh;
-weigh;
-how;
-much;
-fow;
-you;
-weigh;
-watch;
-watch;
-we;
-watched;
-the;
-movie in thearter;
-wash;
-wash;
-i;
-washed;
-you;
-clothes;
-WakeLock;
-walke;
-walke;
-wait;
-wait, wait, walke, wait, vote, vomite, volunteer,
-    volunteer, use, unpake, unpake, type, turn, tunr, ;
-try { }
-finally { }
-travel, transfer, touch, tie, touch, thanke, thanke, thanke;
-you, tanke;
-thanke, tie, taste, talk, talk, talk, swallow,
-    swallow, submit, submit, swallow, study, study, study, stop, stay,
-    stay, start, spell, sort, solve, slove, solve, solve, sneeze,
-    sneeze, smoke, smile, smile, smell, sign, shorten, shorten, shorten, shop, shave, shave, share, shave, shorten, serve, serve, serve,  == separate == , select, schedule, schedule, save, ;
-return ,
-    retire, retire, retire, request, request, request, request, request, ;
-return , retire, request, request, remove, remiain, remain, remain, remain, remain, remain, relax, relax, register,
-    register, register, register, relax, increase, increase,
-    in_crease, in_crease, increase, increase,
-    register, register, register, register, register, reduce, reducer, reduce, recycle, recycle, recorde,
-    raise, reaise, push, pull, protect, print, pretentm,
-    press, prepare, praise;
+// console.log((<string>xs).length);
+// let value : string | undefined | null = null
+// value = 'hellow';
+// wipe;he wiped his hands on a clean towel,
+// weigh weigh weigh weigh how much fow you weigh
+// watch watch we watched the movie in thearter
+// wash wash i washed you clothes WakeLock walke 
+// walke wait wait,wait,walke,wait,vote,vomite,volunteer,
+// volunteer,use,unpake,unpake,type,turn,tunr,try,travel,transfer,touch,tie,touch,thanke,thanke,thanke you,tanke
+// thanke,tie,taste,talk,talk,talk,swallow,
+// swallow,submit,submit,swallow,study,study,study,stop,stay,
+// stay,start,spell,sort,solve,slove,solve,solve,sneeze,
+// sneeze,smoke,smile,smile,smell,sign,shorten,shorten,shorten,shop,shave,shave,share,shave,shorten,serve,serve,serve,==separate==,select,schedule,schedule,save,return,
+// retire,retire,retire,request,request,request,request,request,return,retire,request,request,remove,remiain,remain,remain,remain,remain,remain,relax,relax,register,
+// register,register,register,relax,increase,increase,
+// in_crease,in_crease,increase,increase,
+// register,register,register,register,register,reduce,reducer,reduce,recycle,recycle,recorde,
+// raise,reaise,push,pull,protect,print,pretentm,
+// press,prepare,praise
+// 18:42 => start
+var body = document.body;
+var slides = document.querySelectorAll(".slide");
+var leftBtn = document.getElementById("left");
+var rightBtn = document.getElementById("right");
+var activeSlide = 0;
+rightBtn === null || rightBtn === void 0 ? void 0 : rightBtn.onclick = function () {
+    activeSlide++;
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0;
+    }
+    setVgToBody();
+    setActiveSlide();
+};
+leftBtn.onclick = function () {
+    activeSlide--;
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1;
+    }
+    setVgToBody();
+    setActiveSlide();
+};
+setBgToBody();
+function setVgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+function setActiveSlide() {
+    slides.forEach(function (slide) { return slide.classList.remove('active'); });
+    slides[activeSlide].classList.add('active');
+}
--- day25/app.ts
+++ day25/app.ts
@@ -25,25 +25,66 @@ let x: unknown = 4;
 console.log((x as string).length); // prints undefined since numbers don't have a length
 
 // casting with <>
-console.log((<string>xs).length);
+// console.log((<string>xs).length);
 
-let value : string | undefined | null = null
-value = 'hellow';
+// let value : string | undefined | null = null
+// value = 'hellow';
 
-wipe;he wiped his hands on a clean towel,
-weigh weigh weigh weigh how much fow you weigh
-watch watch we watched the movie in thearter
-wash wash i washed you clothes WakeLock walke 
-walke wait wait,wait,walke,wait,vote,vomite,volunteer,
-volunteer,use,unpake,unpake,type,turn,tunr,try,travel,transfer,touch,tie,touch,thanke,thanke,thanke you,tanke
-thanke,tie,taste,talk,talk,talk,swallow,
-swallow,submit,submit,swallow,study,study,study,stop,stay,
-stay,start,spell,sort,solve,slove,solve,solve,sneeze,
-sneeze,smoke,smile,smile,smell,sign,shorten,shorten,shorten,shop,shave,shave,share,shave,shorten,serve,serve,serve,==separate==,select,schedule,schedule,save,return,
-retire,retire,retire,request,request,request,request,request,return,retire,request,request,remove,remiain,remain,remain,remain,remain,remain,relax,relax,register,
-register,register,register,relax,increase,increase,
-in_crease,in_crease,increase,increase,
-register,register,register,register,register,reduce,reducer,reduce,recycle,recycle,recorde,
-raise,reaise,push,pull,protect,print,pretentm,
-press,prepare,praise
+// wipe;he wiped his hands on a clean towel,
+// weigh weigh weigh weigh how much fow you weigh
+// watch watch we watched the movie in thearter
+// wash wash i washed you clothes WakeLock walke 
+// walke wait wait,wait,walke,wait,vote,vomite,volunteer,
+// volunteer,use,unpake,unpake,type,turn,tunr,try,travel,transfer,touch,tie,touch,thanke,thanke,thanke you,tanke
+// thanke,tie,taste,talk,talk,talk,swallow,
+// swallow,submit,submit,swallow,study,study,study,stop,stay,
+// stay,start,spell,sort,solve,slove,solve,solve,sneeze,
+// sneeze,smoke,smile,smile,smell,sign,shorten,shorten,shorten,shop,shave,shave,share,shave,shorten,serve,serve,serve,==separate==,select,schedule,schedule,save,return,
+// retire,retire,retire,request,request,request,request,request,return,retire,request,request,remove,remiain,remain,remain,remain,remain,remain,relax,relax,register,
+// register,register,register,relax,increase,increase,
+// in_crease,in_crease,increase,increase,
+// register,register,register,register,register,reduce,reducer,reduce,recycle,recycle,recorde,
+// raise,reaise,push,pull,protect,print,pretentm,
+// press,prepare,praise
+
+// 18:42 => start
+
+const body = document.body;
+const slides = document.querySelectorAll(".slide")!;
+const leftBtn = document.getElementById("left")!;
+const rightBtn = document.getElementById("right")!;
+
+let activeSlide = 0
+
+rightBtn?.onclick = ()=>{
+    activeSlide++
+
+    if(activeSlide > slides.length - 1){
+        activeSlide = 0
+    }
+    setVgToBody()
+    setActiveSlide()
+}
+
+leftBtn.onclick = ()=>{
+    activeSlide--
+
+    if(activeSlide < 0){
+        activeSlide = slides.length - 1
+    }
+
+        setVgToBody();
+        setActiveSlide();
+}
+
+setBgToBody();
+
+function setVgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+function setActiveSlide() {
+    slides.forEach((slide)=> slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
 
