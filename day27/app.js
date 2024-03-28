b7169d4d7af9c4c420c1f5538b738c1dc2fbcc3a
amirabas <abasaarafi@outlook.com>
2024-03-29 00:58:25 +0330

    finsh lesson login suntax

---
+++ day27/app.js
@@ -0,0 +1,24 @@
+// The Assignment Operator = assigns values
+// The Addition Operator + adds values
+// The Multiplication Operator * multiplies values
+// The Comparison Operator > compares values
+var a, b, c, content;
+a = 22,
+    b = 'string';
+c = true;
+content = 'string';
+c = a + b;
+c = a * a;
+var t;
+// setInterval(()=>{
+//     a = 5
+//     t = a += 5
+//     console.log(t);
+// },200)
+var text1 = "C22";
+var text2 = "B";
+var result = text1 < text2;
+console.log(result);
+if (a <= 255) {
+    console.log('yes this is ok');
+}
---
+++ day27/app.ts
@@ -0,0 +1,40 @@
+// The Assignment Operator = assigns values
+
+// The Addition Operator + adds values
+
+// The Multiplication Operator * multiplies values
+
+// The Comparison Operator > compares values
+
+let a,b,c,content
+a = 22,
+b = 'string'
+c = true
+content = 'string'
+
+c = a + b;
+c = a * a;
+let t 
+
+// setInterval(()=>{
+//     a = 5
+//     t = a += 5
+//     console.log(t);
+// },200)
+
+let text1 = "C22";
+let text2 = "B";
+let result = text1 < text2;
+
+console.log(result);
+
+
+if(a <= 255){
+    console.log('yes this is ok');
+}
+
+// Note that strings are compared alphabetically: ------------- prefect
+let text1 = "What a very ";
+text1 += "nice day";
+
+
