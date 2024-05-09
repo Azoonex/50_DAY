86d126ec744416632b8f0924bcadf9e720ba2676
amirabas <amirjabic@gmail.com>
2024-05-10 02:22:51 +0330

    world important in ts nice

--- day33/app.js
+++ day33/app.js
@@ -5,3 +5,8 @@ console.log(person);
 // conditional type
 // condination type
 // condination type
+// At the heart of most useful program,we have to make decisions based
+// decisions
+// but givern the fact that values cang be easily introspected besad
+// describnle the raltion betwin
+// taht looks little like
