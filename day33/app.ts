50855149ae8ed9b217bddf5dd94719dacf693f91
amirabas <amirjabic@gmail.com>
2024-05-10 02:46:57 +0330

    intersting types in ts

--- day33/app.ts
+++ day33/app.ts
@@ -46,6 +46,20 @@ Greet(dog);
 // taht looks little like several provide / inferre / 
 // initalizing / initializing / initailizing
 // initializing / determining / return / made / expressions /above
-// above / consider / common / provided / candidate / ideally /ideally / we may want zoo to be inferred as animall[] / we may want / we way want zoo to be infreed as an anima / but because /because / explicitly provide the type when no / candidete / explicity provide the type when no one type is a super / snake rhino / Elephoant / direction / also / cases / cases / cases in typescript / this is kown as contextual typeing . contextul / occurs / contenxtual / when expression / expression / implied/ implied by its location / infer  === result / infer / assignment / when it dide so / when it dide so/ when it dide so / when it dide so/ is was able to infer the type of the mouse Evet / Compatibiliy / compatibility / comp ea e amin reza abas / reza / sara / neda amir / abas / amir  /reza / elhame elhame elhame / elhame / abas / compatiility / based / based / based / on strucurel / strucural subtyping  / structural / structural / structural / typeing is way of relating typeing / consider the following code / contrast / contrast / nominal / contreast / nominal / nominal / consider / consider / the foollowing code /
+// above / consider / common / provided / candidate / ideally /ideally / we may want zoo to be inferred as animall[] / we may want / we way want zoo to be infreed as an anima / but because /because / explicitly provide the type when no / candidete / explicity provide the type when no one type is a super / snake rhino / Elephoant / direction / also / cases / cases / cases in typescript / this is kown as contextual typeing . contextul / occurs / contenxtual / when expression / expression / implied/ implied by its location / infer  === result / infer / assignment / when it dide so / when it dide so/ when it dide so / when it dide so/ is was able to infer the type of the mouse Evet / Compatibiliy / compatibility / comp ea e amin reza abas / reza / sara / neda amir / abas / amir  /reza / elhame elhame elhame / elhame / abas / compatiility / based / based / based / on strucurel / strucural subtyping  / structural / structural / structural / typeing is way of relating typeing / consider the following code / contrast / contrast / nominal / contreast / nominal / nominal / consider / consider / the foollowing code / Comparing / Comparing / while primitive  / relatively /given 
 
-// 
+
+
+
+let x = (a:number ) => 0
+let y = (b:number) => 0
+
+y = x;
+x = y;
+
+
+let items = [1,2,3];
+
+items.forEach((items,index,array)=> console.log(items,index,array));
+
+
