d2b41d4783ede935f64e9b97f6f46641d7c6e694
amirabas <amirjabic@gmail.com>
2024-05-14 18:59:16 +0330

    nice for is js method

--- day33/app.js
+++ day33/app.js
@@ -8,7 +8,15 @@ else {
 var fruits = ["banana", "apple", "peach"];
 console.log(Object.keys(fruits)); // increasring / exteds
 fruits.forEach(function (i, o) {
-    console.log((o + 1) + ":", i);
+    console.log(o + 1 + ":", i);
 });
 console.log(fruits.reverse());
-// get start study for javascript Loverhttps://storyset.com/information
+var newFruits = fruits.keys();
+for (var _i = 0, newFruits_1 = newFruits; _i < newFruits_1.length; _i++) {
+    var key = newFruits_1[_i];
+    console.log("".concat(key, " : ").concat(fruits[key]));
+}
+var nums = [-1, 0, 1, 2, -1, -4];
+for (var i = 0; i < nums.length; i++) {
+    console.log(nums[i] + 3);
+}
--- day33/app.ts
+++ day33/app.ts
@@ -1,27 +1,30 @@
+let Years = ["2", "amirabas", "weekind"];
 
-
-
-let Years = ["2","amirabas","weekind"]
-
-if(Years[2].length === 7){
-    console.log("nice ok")
+if (Years[2].length === 7) {
+    console.log("nice ok");
 } else {
-    console.log("error")
-    
+    console.log("error");
 }
 
 const fruits = ["banana", "apple", "peach"];
 
 console.log(Object.keys(fruits)); // increasring / exteds
 
-fruits.forEach((i,o)=>{
-    console.log((o + 1) + ":",i)
-})
+fruits.forEach((i, o) => {
+    console.log(o + 1 + ":", i);
+});
 
-console.log(fruits.reverse())
+console.log(fruits.reverse());
+
+const newFruits = fruits.keys();
+
+for(const key of newFruits){
+    console.log(`${key} : ${fruits[key]}`)
+}
 
 
+let nums = [-1, 0, 1, 2, -1, -4];
 
-
-// get start study for javascript Loverhttps://storyset.com/information
-
+for(let i = 0 ; i < nums.length;i++){
+    console.log(nums[i] + 3 )
+}
