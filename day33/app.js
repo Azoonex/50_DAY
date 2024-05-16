6547c979a7cfa904398f73b2c59470b758b9d4d3
amirabas <amirjabic@gmail.com>
2024-05-16 23:38:59 +0330

    remove worde

--- day33/app.js
+++ day33/app.js
@@ -1,22 +1,20 @@
-var Years = ["2", "amirabas", "weekind"];
-if (Years[2].length === 7) {
-    console.log("nice ok");
-}
-else {
-    console.log("error");
-}
-var fruits = ["banana", "apple", "peach"];
-console.log(Object.keys(fruits)); // increasring / exteds
-fruits.forEach(function (i, o) {
-    console.log(o + 1 + ":", i);
-});
-console.log(fruits.reverse());
-var newFruits = fruits.keys();
-for (var _i = 0, newFruits_1 = newFruits; _i < newFruits_1.length; _i++) {
-    var key = newFruits_1[_i];
-    console.log("".concat(key, " : ").concat(fruits[key]));
-}
-var nums = [-1, 0, 1, 2, -1, -4];
-for (var i = 0; i < nums.length; i++) {
-    console.log(nums[i] + 3);
-}
+// let Years = ["2", "amirabas", "weekind"];
+// if (Years[2].length === 7) {
+//     console.log("nice ok");
+// } else {
+//     console.log("error");
+// }
+// const fruits = ["banana", "apple", "peach"];
+// console.log(Object.keys(fruits)); // increasring / exteds
+// fruits.forEach((i, o) => {
+//     console.log(o + 1 + ":", i);
+// });
+// console.log(fruits.reverse());
+// const newFruits = fruits.keys();
+// for(const key of newFruits){
+//     console.log(`${key} : ${fruits[key]}`)
+// }
+// let nums = [-1, 0, 1, 2, -1, -4];
+// for(let i = 0 ; i < nums.length;i++){
+//     console.log(nums[i] + 3 )
+// }
