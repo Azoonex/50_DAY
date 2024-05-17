45dfe22d96841ac233a428277a332a76cb275881
amirabas <amirjabic@gmail.com>
2024-05-18 03:04:09 +0330

    world 20 =^ 12/5

--- day33/app.js
+++ day33/app.js
@@ -18,3 +18,50 @@
 // for(let i = 0 ; i < nums.length;i++){
 //     console.log(nums[i] + 3 )
 // }
+// Array Search
+var fruits = ["Apple", "Orange", "Banana", "Mango"];
+// let position = fruits.indexOf("Apple");
+// let positionnew = fruits.lastIndexOf("Apple");
+for (var _i = 0, fruits_1 = fruits; _i < fruits_1.length; _i++) {
+    var fru = fruits_1[_i];
+    switch (fru) {
+        case "Apple":
+            console.log("reza");
+            break;
+        case "5":
+            console.log("True");
+            break;
+        default:
+            console.log("unknowfrutes");
+            break;
+    }
+}
+if (fruits.includes("Apple")) {
+    console.log("this is bast frutis");
+}
+// const numbers = [4, 9, 16, 25, 29];
+// let first = numbers.find(myFunction);
+// interface TypeNumber {
+//     value: number;
+//     index: number;
+// }
+// async function myFunction({ value, index }: TypeNumber) {
+//     return value > 18;
+// }
+// myFunction({value: 1,index: 1})
+// const numbers = [4, 9, 16, 25, 29];
+// interface TypeNumber {
+//     value: number;
+//     index: number;
+// }
+// let first = numbers.find((value, index) => myFunction({ value, index }));
+// function myFunction({ value, index }: TypeNumber): boolean {
+//     return value > 18;
+// }
+// console.log(myFunction({ value: 1, index: 1 }));
+var numbers = [4, 9, 16, 25, 29];
+var first = numbers.find(myFunction);
+function myFunction() {
+    return function (value, index) { return value > 18; };
+}
+console.log(myFunction(1, 1));
--- day33/app.ts
+++ day33/app.ts
@@ -22,13 +22,86 @@
 //     console.log(`${key} : ${fruits[key]}`)
 // }
 
-
 // let nums = [-1, 0, 1, 2, -1, -4];
 
 // for(let i = 0 ; i < nums.length;i++){
 //     console.log(nums[i] + 3 )
 // }
 
+// Array Search
 
+const fruits = ["Apple", "Orange", "Banana", "Mango"];
+
+// let position = fruits.indexOf("Apple");
+// let positionnew = fruits.lastIndexOf("Apple");
+
+for (const fru of fruits) {
+    switch (fru) {
+        case "Apple":
+            console.log("reza");
+            break;
+        case "5":
+            console.log("True");
+            break;
+        default:
+            console.log("unknowfrutes");
+            break;
+    }
+}
+
+if (fruits.includes("Apple")) {
+    console.log("this is bast frutis");
+}
+
+// const numbers = [4, 9, 16, 25, 29];
+
+// let first = numbers.find(myFunction);
+
+// interface TypeNumber {
+//     value: number;
+//     index: number;
+// }
+
+// async function myFunction({ value, index }: TypeNumber) {
+//     return value > 18;
+// }
+
+// myFunction({value: 1,index: 1})
+
+// const numbers = [4, 9, 16, 25, 29];
+
+// interface TypeNumber {
+//     value: number;
+//     index: number;
+// }
+
+// let first = numbers.find((value, index) => myFunction({ value, index }));
+
+// function myFunction({ value, index }: TypeNumber): boolean {
+//     return value > 18;
+// }
+
+// console.log(myFunction({ value: 1, index: 1 }));
+
+
+// const numbers = [4, 9, 16, 25, 29];
+
+// interface TypeNumberCallback {
+//     (value: number, index: number): boolean;
+// }
+
+// let first = numbers.find(myFunction);
+
+// function myFunction: TypeNumberCallback {
+//     return (value, index) => value > 18;
+// }
+
+// console.log(myFunction(1, 1));
+
+// new exam
+
+"afride agree angray arive attack bottom cleaver curle finaley hide hunt lot middle moment plase promise replae sife trick well advendure aproage crfulle cimciale create evele exprement kill labeletore lugth loud nerves "
+
+"adventure adventure approach approcach carefully carefully chemical create evil experiment laboratory laboratory length length length length nervece"
 
 
