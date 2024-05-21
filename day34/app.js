60180330522d065bf9f64f5816b71ff752574458
amirabas <amirjabic@gmail.com>
2024-05-21 15:52:50 +0330

    flat : create 3 algoritem for isPrime

--- day34/app.js
+++ day34/app.js
@@ -1,24 +1,40 @@
 // https://www.youtube.com/playlist?list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP
-function fibonacci(n) {
-    var fib = [0, 1];
-    for (var i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
+// function fibonacci(n: number): number[] {
+//     const fib = [0, 1];
+//     for (let i = 2; i < n; i++) {
+//         fib[i] = fib[i - 1] + fib[i - 2];
+//     }
+//     return fib;
+// }
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(7));
+// console.log(fibonacci(12));
+// // function 2
+// function factorial(n: number): number {
+//     let b = 1;
+//     for (let i = 2; i <= n; i++) {
+//         b = b * i;
+//     }
+//     return b;
+// }
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(4));
+// console.log(factorial(5));
+// Prime number
+function isPrime(prime) {
+    if (prime < 2) {
+        return false;
     }
-    return fib;
-}
-console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(7));
-console.log(fibonacci(12));
-// function 2
-function factorial(n) {
-    var b = 1;
-    for (var i = 2; i <= n; i++) {
-        b = b * i;
+    var sqrtPrime = Math.sqrt(prime);
+    for (var i = 2; i <= sqrtPrime; i++) {
+        if (prime % i === 0) {
+            return false; // Not a prime number
+        }
     }
-    return b;
+    return true; // Prime number
 }
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(4));
-console.log(factorial(5));
+console.log(isPrime(1));
+console.log(isPrime(5));
+console.log(isPrime(4));
--- day34/app.ts
+++ day34/app.ts
@@ -1,29 +1,51 @@
 // https://www.youtube.com/playlist?list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP
 
-function fibonacci(n: number): number[] {
-    const fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
+// function fibonacci(n: number): number[] {
+//     const fib = [0, 1];
+//     for (let i = 2; i < n; i++) {
+//         fib[i] = fib[i - 1] + fib[i - 2];
+//     }
+//     return fib;
+// }
+
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(7));
+// console.log(fibonacci(12));
+
+// // function 2
+
+// function factorial(n: number): number {
+//     let b = 1;
+//     for (let i = 2; i <= n; i++) {
+//         b = b * i;
+//     }
+//     return b;
+// }
+
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(4));
+// console.log(factorial(5));
+
+// Prime number
+
+function isPrime(prime: number): boolean {
+    if (prime < 2) {
+        return false;
     }
-    return fib;
+
+    let sqrtPrime: number = Math.sqrt(prime);
+    for (let i = 2; i <= sqrtPrime; i++) {
+        if (prime % i === 0) {
+            return false; // Not a prime number
+        }
+    }
+
+    return true; // Prime number
 }
 
-console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(7));
-console.log(fibonacci(12));
+console.log(isPrime(1));
+console.log(isPrime(5));
+console.log(isPrime(4));
 
-// function 2
-
-function factorial(n: number): number {
-    let b = 1;
-    for (let i = 2; i <= n; i++) {
-        b = b * i;
-    }
-    return b;
-}
-
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(4));
-console.log(factorial(5));
