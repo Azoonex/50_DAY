681675a3ca4b40cc821222d796a990cb792e8591
amirabas <amirjabic@gmail.com>
2024-06-06 21:23:35 +0330

    what work validation form in html  how work of javascript

--- day35/app.js
+++ day35/app.js
@@ -1,11 +1,28 @@
-function recrsiveFactorial(n) {
-    if (n === 0) {
-        return 1;
-    }
-    return n * recrsiveFactorial(n - 1);
-}
-console.log(recrsiveFactorial(0));
-console.log(recrsiveFactorial(1));
-console.log(recrsiveFactorial(5));
-function linearSearch(arr, target) {
-}
+// function recrsiveFactorial( n:number ) {
+//     if(n === 0 ){
+//         return 1
+//     }
+//     return n * recrsiveFactorial(n-1)
+// }
+// console.log(recrsiveFactorial(0));
+// console.log(recrsiveFactorial(1));
+// console.log(recrsiveFactorial(5));
+// function linearSearch(arr,target){
+// }
+// below are some expamples of how you can use the docment object t o access and manipulate html. maniuplate manipulate html
+// changing innerhtml 
+// createElement
+// removechild()
+// document.x.removechild
+// document.appendchild
+// replacechild(h1,x)
+// click document.getelementbyid(id).
+// finding html object
+// form vaildtion can be done by from vailditon can be done by js
+// done by done by
+// process of ensuringprocess ensuring essiringthat user input is celan correct and useft that => که / process of ensuing user input is clean typical validation tasks are . typincal validation tasks are 
+// has the user filled in all required fields 
+// has the user entered a valida date ?
+// has the user entered text a numeric field
+// ensure ensure ensure ensure
+// can be defined by many different methods and deplyoued in many different ways can be defined by many deffereent methos and deployed in may diffrenet ways definded defined defined defined definded by many differeent methos and been been been been been performed 
--- day35/app.ts
+++ day35/app.ts
@@ -24,4 +24,14 @@
 
 
 // click document.getelementbyid(id).
-// 
+// finding html object
+// form vaildtion can be done by from vailditon can be done by js
+// done by done by
+// process of ensuringprocess ensuring essiringthat user input is celan correct and useft that => که / process of ensuing user input is clean typical validation tasks are . typincal validation tasks are 
+
+// has the user filled in all required fields 
+// has the user entered a valida date ?
+// has the user entered text a numeric field
+// ensure ensure ensure ensure
+// can be defined by many different methods and deplyoued in many different ways can be defined by many deffereent methos and deployed in may diffrenet ways definded defined defined defined definded by many differeent methos and been been been been been performed 
+
