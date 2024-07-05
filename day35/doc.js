346d3b626f7f0b17efd856545ebb4a35adfd2360
amirabas <amirjabic@gmail.com>
2024-07-05 13:56:02 +0330

    js array

---
+++ day35/doc.js
@@ -0,0 +1,66 @@
+// let arr = ["I", "go", "home"];
+
+// delete arr[2];
+
+// console.log(arr[2])
+
+// const arr2  =arr.splice(1,2)
+// console.log(arr2)
+
+// let arr = ["I", "study", "JavaScript", "right", "now"];
+// how work deleted and splice
+
+// arr.splice(0,3,'ts','vue','reza');
+// console.log(arr)
+
+// let arr2 = ['i','study','js'];
+
+// arr2.splice(2,0,"c#",'goling')
+
+// console.log(arr2);
+
+// let numb = [1, 2, 5];
+
+// numb.splice(-1,0,3,4)
+
+// console.log(numb)
+
+
+// let arr = ["t", "e", "s", "t"];
+
+// console.log(arr.slice(1,4));
+// console.log(arr.slice(-2))
+
+// let names = ['amir'];
+// let lastName = ['rezia']
+
+
+
+// console.log(names.concat(lastName,'amirreza bigsafe'))
+// let arr = [1, 2];
+
+// let arrayLike = {
+//     0: "something",
+//     1: "else",
+//     [Symbol.isConcatSpreadable]: true,
+//     length: 2
+// };
+
+// console.log(arr.concat(arrayLike));
+
+// leterate
+
+// ["Bilbo", "Gandalf", "Nazgul"].forEach((item,index, ary)=>{
+//     console.log(`${item} is at index ${index} in ${ary}`)
+// });
+
+// SEARCH IN ARRAY
+
+// let arr = [1, 0, false];
+
+// console.log(arr.indexOf(null))
+// console.log(arr.includes(1))
+
+// let fruits = ['Apple', 'Orange', 'Apple']
+
+// console.log(fruits.indexOf("Apple"))
