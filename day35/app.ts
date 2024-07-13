5a0936d6cbca74b145a471fb3607fc3caeb5821a
amirabas <amirjabic@gmail.com>
2024-07-13 06:30:36 +0330

    none types

--- day35/app.ts
+++ day35/app.ts
@@ -8,15 +8,45 @@ let newSize: Size = Size.medium;
 
 console.log(newSize);
 
-function checkNumber(num: number,options= "amirabas"): string {
+function checkNumber(num: number, options = "amirabas"): string {
     if (num) {
         return "amirabas";
     }
     if (num === undefined) {
         return options;
     }
-    
+
     return "default value";
 }
 
-checkNumber(22)
+checkNumber(22);
+
+function proccedItems(item: string | number): number {
+    if (typeof item === "string") {
+        console.log("amir reza");
+        return 1;
+    }
+    if (typeof item === "number") {
+        console.log("amirabas");
+        return 2;
+    }
+    return 2;
+}
+
+
+type whide = {
+    uiSize: () => void
+}
+
+type hight = {
+    uxSize: () => void
+}
+
+let fun: whide & hight = {
+    uiSize(){
+
+    },
+    uxSize() {
+        
+    },
+}
