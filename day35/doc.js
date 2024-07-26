00916bd1c2ce1c4b1b8fb5d69930118ea16b3fef
amirabas <amirjabic@gmail.com>
2024-07-26 20:01:53 +0330

    how work Soluation 2 for traingle

--- day35/doc.js
+++ day35/doc.js
@@ -10,29 +10,38 @@ var daylist = [
     "Friday",
     "Saturday",
 ];
-console.log('Today is:' + daylist[day] + ".");
+console.log("Today is:" + daylist[day] + ".");
 var houre = today.getHours();
 var minute = today.getMinutes();
 var second = today.getSeconds();
-var prepand = (houre >= 12) ? " PM" : "Am";
-if (houre === 0 && prepand === ' PM') {
+var prepand = houre >= 12 ? " PM" : "Am";
+if (houre === 0 && prepand === " PM") {
     if (minute === 0 && second === 0) {
         houre = 12;
-        prepand = ' Noon';
+        prepand = " Noon";
     }
     else {
         houre = 12;
-        prepand = ' PM';
+        prepand = " PM";
     }
 }
-if (houre === 0 && prepand === ' AM ') {
+if (houre === 0 && prepand === " AM ") {
     if (minute === 0 && second === 0) {
         houre = 12;
-        prepand = ' midnight';
+        prepand = " midnight";
     }
     else {
         houre = 12;
-        prepand = ' AM ';
+        prepand = " AM ";
     }
 }
-console.log('Current Time:' + houre + prepand + ":" + minute + ":" + second);
+console.log("Current Time:" + houre + prepand + ":" + minute + ":" + second);
+// Explantions explanations Explanations Explanations Explantation
+// js find the ara of a triangle where lenght of the three of its sides calculates calculats the ara of a triangle with sides
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+var s = (side1 + side2 + side3) / 2;
+var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+console.log(area);
+//JavaScript Basic: Exercise-4 with Solution program to find the area of a triangle 
--- day35/doc.ts
+++ day35/doc.ts
@@ -1,9 +1,8 @@
 // js determines the day of the week , and formats the current house
 
-let today = new Date()
+let today = new Date();
 
-let day = today.getDay()
-
+let day = today.getDay();
 
 let daylist = [
     "Sunday",
@@ -15,35 +14,49 @@ let daylist = [
     "Saturday",
 ];
 
-console.log('Today is:' + daylist[day] + ".");
+console.log("Today is:" + daylist[day] + ".");
 
-let houre= today.getHours();
+let houre = today.getHours();
 let minute = today.getMinutes();
 let second = today.getSeconds();
 
-let prepand = (houre>= 12) ? " PM" : "Am";
+let prepand = houre >= 12 ? " PM" : "Am";
 
-if(houre=== 0 && prepand === ' PM'){
-    if(minute === 0 && second === 0){
-        houre= 12;
-        prepand = ' Noon'
+if (houre === 0 && prepand === " PM") {
+    if (minute === 0 && second === 0) {
+        houre = 12;
+        prepand = " Noon";
     } else {
-        houre= 12;
-        prepand = ' PM'
+        houre = 12;
+        prepand = " PM";
     }
 }
 
-if (houre === 0 && prepand === ' AM '){
-    if(minute === 0 && second === 0){
+if (houre === 0 && prepand === " AM ") {
+    if (minute === 0 && second === 0) {
         houre = 12;
-        prepand = ' midnight'
-    }else {
+        prepand = " midnight";
+    } else {
         houre = 12;
-        prepand = ' AM '
+        prepand = " AM ";
     }
 }
 
-console.log('Current Time:' + houre + prepand + ":" + minute + ":" + second);
+console.log("Current Time:" + houre + prepand + ":" + minute + ":" + second);
 
 // Explantions explanations Explanations Explanations Explantation
 
+// js find the ara of a triangle where lenght of the three of its sides calculates calculats the ara of a triangle with sides
+
+let side1 = 5;
+let side2 = 6;
+let side3 = 7;
+
+
+let s = (side1 + side2 + side3 ) / 2;
+
+let area = Math.sqrt(s * ((s-side1) * (s - side2) * (s - side3)))
+
+console.log(area)
+
+//JavaScript Basic: Exercise-4 with Solution program to find the area of a triangle 
