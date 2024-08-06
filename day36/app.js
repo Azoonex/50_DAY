d6e57e5478b292df5f6709f26b1ae23c3e2902cd
amirabas <amirjabic@gmail.com>
2024-08-07 00:35:33 +0330

    solve a new problem solation 16

--- day36/app.js
+++ day36/app.js
@@ -12,9 +12,9 @@
 // if(demoTage){
 //     animate_string(demoTage)
 // }
-// 6 : Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.  
+// 6 : Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
 // lightweight lightweight lanuage language language inside provide provide programmatic approach
-// to check if the 1st january of a year is a sunday we can use the date obect js create a date object for the 1 set january of each year and 
+// to check if the 1st january of a year is a sunday we can use the date obect js create a date object for the 1 set january of each year and
 // for(let year = 2014 ; year <= 2050;year++){
 //     let d = new Date(year,0,1)
 //     if(d.getDay() === 0){
@@ -27,7 +27,7 @@
 // let progmet = 10
 // if(num === progmet){
 //     console.log('this nice work!')
-// } else{ 
+// } else{
 //     console.log('not matched ,the number was' + num);
 // }
 // remain . kill . reach . remain . cut . fall . stay . build . build . expect . sende . die . serve . wait . serve . die . send . wait . buy . appear . consider . appear . buy . wait . love . remember . offer . win . walk . walk . open .grow . spend . add .allow . win . offer . remember . love consider . consider appear . but . read . speak create . stop . follow . watch understand . lead . change . learn set continue . incluse meet pay lose stand . sit . provide . write . bring . hold hold . believe . live . like happen . must . provide . sit .stand .lose .pay . live . like write . provide . sit . stand . lose . pay . meet include . continue . set . lean
@@ -39,10 +39,10 @@ if (today.getMonth() === 11 && today.getDay() > 25) {
 var one_day = 1000 * 60 * 60 * 24;
 console.log(Math.ceil((cmas.getTime() - today.getTime()) / one_day) +
     " days left until Christmas!");
-var filename = 'sstem.php.token.base.createing.bonde';
-console.log(filename.split('.'));
+var filename = "sstem.php.token.base.createing.bonde";
+console.log(filename.split("."));
 var names = "search&search&items&city&specialty";
-console.log(names.split('&'));
+console.log(names.split("&"));
 function difference(n) {
     if (n <= 13) {
         return 13 - n;
@@ -54,7 +54,20 @@ function difference(n) {
 console.log(difference(55));
 // solation 15 create
 function testhundred(num) {
-    var news = ((Math.abs(100 - num) <= 20) || (Math.abs(400 - num) <= 20));
+    var news = Math.abs(100 - num) <= 20 || Math.abs(400 - num) <= 20;
     return news;
 }
 console.log(testhundred(5));
+function sum(_a) {
+    var numone = _a.numone, numtwo = _a.numtwo;
+    if (numone === numtwo) {
+        return 3 * (numone + numtwo);
+    }
+    else {
+        return numone + numtwo;
+    }
+}
+console.log(sum({
+    numone: 22,
+    numtwo: 22,
+}));
--- day36/app.ts
+++ day36/app.ts
@@ -17,12 +17,11 @@
 //     animate_string(demoTage)
 // }
 
-// 6 : Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.  
-
+// 6 : Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
 
 // lightweight lightweight lanuage language language inside provide provide programmatic approach
 
-// to check if the 1st january of a year is a sunday we can use the date obect js create a date object for the 1 set january of each year and 
+// to check if the 1st january of a year is a sunday we can use the date obect js create a date object for the 1 set january of each year and
 
 // for(let year = 2014 ; year <= 2050;year++){
 //     let d = new Date(year,0,1)
@@ -40,51 +39,79 @@
 
 // if(num === progmet){
 //     console.log('this nice work!')
-// } else{ 
+// } else{
 //     console.log('not matched ,the number was' + num);
 // }
 // remain . kill . reach . remain . cut . fall . stay . build . build . expect . sende . die . serve . wait . serve . die . send . wait . buy . appear . consider . appear . buy . wait . love . remember . offer . win . walk . walk . open .grow . spend . add .allow . win . offer . remember . love consider . consider appear . but . read . speak create . stop . follow . watch understand . lead . change . learn set continue . incluse meet pay lose stand . sit . provide . write . bring . hold hold . believe . live . like happen . must . provide . sit .stand .lose .pay . live . like write . provide . sit . stand . lose . pay . meet include . continue . set . lean
 
-let today  = new Date()
+let today = new Date();
 
-var cmas = new Date(today.getFullYear(),11,25)
+var cmas = new Date(today.getFullYear(), 11, 25);
 
-if(today.getMonth() === 11 && today.getDay() > 25){
-    cmas.setFullYear(cmas.getFullYear() + 1)
+if (today.getMonth() === 11 && today.getDay() > 25) {
+    cmas.setFullYear(cmas.getFullYear() + 1);
 }
-
 
 var one_day = 1000 * 60 * 60 * 24;
 
 console.log(
     Math.ceil((cmas.getTime() - today.getTime()) / one_day) +
         " days left until Christmas!"
-); 
+);
 
-let filename = 'sstem.php.token.base.createing.bonde'
+let filename = "sstem.php.token.base.createing.bonde";
 
-console.log(filename.split('.'))
-let names = "search&search&items&city&specialty"
+console.log(filename.split("."));
+let names = "search&search&items&city&specialty";
 
-console.log(names.split('&'))
+console.log(names.split("&"));
 
-function difference(n:number){
-    if(n <= 13){
-        return 13 -n
-    } else { 
-        return (n - 13) * 2
+function difference(n: number) {
+    if (n <= 13) {
+        return 13 - n;
+    } else {
+        return (n - 13) * 2;
     }
 }
 
 console.log(difference(55));
 
-
 // solation 15 create
 
 function testhundred(num: number) {
-    let news = ((Math.abs(100 - num) <= 20) || (Math.abs(400 - num) <= 20))
+    let news = Math.abs(100 - num) <= 20 || Math.abs(400 - num) <= 20;
 
-    return news
+    return news;
 }
 
-console.log(testhundred(5))
+console.log(testhundred(5));
+// compute to compute the sum of the two given integers if the two value are the same,then return triple their sum to triple
+// same / triple / their / demonstrates / conditional / determine / whether / equal / perform / appropriate / compuation / compuation / accordingly / acoordingly
+
+type Typesum = {
+    numone: number;
+    numtwo: number;
+};
+
+function sum({ numone, numtwo }: Typesum) {
+    if (numone === numtwo) {
+        return 3 * (numone + numtwo);
+    } else {
+        return numone + numtwo;
+    }
+}
+
+console.log(
+    sum({
+        numone: 22,
+        numtwo: 22,
+    })
+);
+
+// algorithm solatiion 16
+// () function sum
+// <> x === y
+// [] return 3 * (x + y)
+// [] return (x + y)
+// END
+
