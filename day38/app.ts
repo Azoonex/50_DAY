b3b3564bcf2d9d2fb30123c7dbfd6ed46e4cf3f6
azoonex <amirjabic@gmail.com>
2024-08-21 10:01:39 +0330

    create for fetching data

--- day38/app.ts
+++ day38/app.ts
@@ -1,2 +1,21 @@
 // waht is clean code ?
 
+function notFound (){
+    return false
+}
+
+// fetch server components
+async function fetchData() {
+    let res = await fetch(`https://localhost.com/api/v1/components`);
+    const data = res.ok ? await res.json() : null;
+
+
+    if(!data || data?.status === 0){
+        return notFound()
+    } return true
+
+}
+
+
+// nice fetching data 
+
