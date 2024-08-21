05865f9151dc7ddbbde3be5160c3d80402c576af
azoonex <amirjabic@gmail.com>
2024-08-21 15:43:36 +0330

    solve the new exersice 23

--- day38/app.ts
+++ day38/app.ts
@@ -27,18 +27,26 @@ formmatedUrl = formmatedUrl.substring(formmatedUrl.lastIndexOf('/') + 1)
 
 // solation 42
 
-function checkThreeNumber(x:number,y:number,z:number){
-        if(y > x && z > y){
-            return 'strict mode';
-        }else if(z > y){
-            return 'Soft mode';
-        } return 'undefinde'
+function checkThreeNumber(x: number, y: number, z: number) {
+    if (y > x && z > y) {
+        return 'strict mode';
+    } else if (z > y) {
+        return 'Soft mode';
+    } return 'undefinde'
 
 }
 
-console.log(checkThreeNumber(22,33,54))
+console.log(checkThreeNumber(22, 33, 54))
 
-// 
+// soaltion 43
 
+function lessby20_others(x: number, y: number, z: number) {
+    const isXEligible = x >= 20 && (x < y || x < z);
+    const isYEligible = y >= 20 && (y < x || y < z);
+    const isZEligible = z >= 20 && (z < x || z < y);
 
+    return isXEligible || isYEligible || isZEligible;
+}
+
+console.log(lessby20_others(33, 44, 55))
 
