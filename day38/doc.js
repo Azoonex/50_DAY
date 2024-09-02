479e8073e811e1c3cedf53d5bcc7bea1ed7476a5
amirabas <amirjabic@gmail.com>
2024-09-02 19:46:24 +0330

    solive the solutaion 45

--- day38/doc.js
+++ day38/doc.js
@@ -1,11 +1,27 @@
 function repetChar(s) {
     if (s.length <= 3)
-        return 'this is not Sentence';
-    return (/a...b/).test(s) || (/b...a/).test(s);
+        return "this is not Sentence";
+    return /a...b/.test(s) || /b...a/.test(s);
 }
-console.log(repetChar('amirabas'));
+console.log(repetChar("amirabas"));
 // separated separeaeted separtated exactly exactly
 function counterVowels(str) {
     return str.replace(/^aeiou/, "").length;
 }
-// solve th
+// solation
+function counterVowelsNew(str) {
+    return str.replace(/aiuoe/, "").length;
+}
+console.log(counterVowelsNew("amirabas"));
+// solution 55
+function checkTP(str) {
+    var str_p = str.replace(/[^p]/g, "");
+    var str_t = str.replace(/[^t]/g, "");
+    var pnum = str_p.length;
+    var tnum = str_t.length;
+    if (pnum === tnum)
+        return true;
+    return false;
+}
+console.log("chckTp" + checkTP('amirabas top levele p and bastprogram'));
+//ss
--- day38/doc.ts
+++ day38/doc.ts
@@ -1,16 +1,36 @@
-function repetChar(s:string){
-    if(s.length <= 3) return 'this is not Sentence'
+function repetChar(s: string) {
+    if (s.length <= 3) return "this is not Sentence";
 
-    return (/a...b/).test(s) || (/b...a/).test(s);
+    return /a...b/.test(s) || /b...a/.test(s);
 }
 
-console.log(repetChar('amirabas'))
+console.log(repetChar("amirabas"));
 
 // separated separeaeted separtated exactly exactly
 
-
-function counterVowels(str : string){
-    return str.replace(/^aeiou/,"").length
+function counterVowels(str: string) {
+    return str.replace(/^aeiou/, "").length;
 }
 
-// solve th
+// solation
+
+function counterVowelsNew(str: string) {
+    return str.replace(/aiuoe/, "").length;
+}
+
+console.log(counterVowelsNew("amirabas"));
+
+// solution 55
+
+function checkTP(str: string) {
+    let str_p = str.replace(/[^p]/g, "");
+    let str_t = str.replace(/[^t]/g, "");
+
+    let pnum = str_p.length;
+    let tnum = str_t.length;
+
+    if (pnum === tnum) return true;
+    return false;
+}
+console.log("chckTp" + checkTP('amirabas top levele p and bastprogram'));
+//ss
