61dc55aa51cd2909057f4aec67fbe1574fbf0e66
amirabas <amirjabic@gmail.com>
2024-09-18 21:20:15 +0330

    solve the solution 36

--- day38/type.js
+++ day38/type.js
@@ -3,15 +3,26 @@ function content(xs, u) {
 }
 var mergeObject = content({
     name: "mmade",
-    hibbites: "male"
+    hibbites: "male",
 }, {
-    age: 33
+    age: 33,
 });
 function cehckEndString(str) {
-    if (str.length <= 6)
-        return false;
-    var Scr = "Script";
-    return str.slice(-6) === Scr;
+    if (str.substring(str.length - 6, str.length) === "Script") {
+        return true;
+    }
+    return false;
+    // 7 - 6 1,7
 }
-console.log(cehckEndString("javaScript"));
+console.log(cehckEndString("amirabas"));
 // solve the new solution 65
+function checknameCity(str) {
+    if (str.length <= 3)
+        return false;
+    else if (str.substring(0, 3) === "Los" || str.substring(0, 3) === "Now") {
+        return true;
+    }
+    return false;
+}
+console.log(checknameCity("Now"));
+// solve the solution 36
--- day38/type.ts
+++ day38/type.ts
@@ -1,24 +1,36 @@
-function content<T,U>(xs:T,u:U){
-    return Object.assign(xs,u)
+function content<T, U>(xs: T, u: U) {
+    return Object.assign(xs, u);
 }
 
-const mergeObject = content({
-    name : "mmade",
-    hibbites : "male"
-},{
-    age : 33
-})
+const mergeObject = content(
+    {
+        name: "mmade",
+        hibbites: "male",
+    },
+    {
+        age: 33,
+    }
+);
 
-function cehckEndString(str:string){
-    if(str.length <= 6) return false;
+function cehckEndString(str: string) {
+    if (str.substring(str.length - 6, str.length) === "Script") {
+        return true;
+    }
+    return false;
 
-    let Scr = "Script"
-    
-    return str.slice(-6) === Scr
-
-
-    
+    // 7 - 6 1,7
 }
-console.log(cehckEndString("javaScript"))
+console.log(cehckEndString("amirabas"));
 // solve the new solution 65
 
+function checknameCity(str: string) {
+    if (str.length <= 3) return false;
+    else if (str.substring(0, 3) === "Los" || str.substring(0, 3) === "Now") {
+        return true;
+    }
+    return false;
+}
+
+console.log(checknameCity("Now"));
+
+// solve the solution 36
