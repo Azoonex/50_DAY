d8dd2d753044aa0d8466537e961622d6e6e81b21
amirabas <amirjabic@gmail.com>
2024-10-30 08:32:07 +0330

    how work opreator

--- day39/doc.js
+++ day39/doc.js
@@ -2,27 +2,21 @@
 // const btn: HTMLElement | null = document.getElementById("button");
 // const demo = document.getElementById("demo")! as HTMLElement;
 // const btnStopDate = document.getElementById("stopDate")!;
-
 // const myInterVale = setInterval(setDate, 1000);
-
 // function setDate(): number | void {
 //     const date = new Date();
 //     demo.innerHTML = date.toLocaleString();
 //     console.log(date.toLocaleString());
 // }
-
 // btnStopDate.onclick = handleStop;
-
 // function handleStop() {
 //     clearInterval(myInterVale);
 // }
-
 // function mymove() {
 //     let id: number | null = null;
 //     let pos = 0;
 //     if (id) clearInterval(id);
 //     id = setInterval(famer, 5);
-
 //     function famer() {
 //         if (pos === 500) {
 //             if (id) clearInterval(id);
@@ -37,17 +31,12 @@
 //         }
 //     }
 // }
-
 // if (btn) btn.onclick = mymove;
-
-const item = 10;
-const teamp = String(10)
-const teamp2 = String([1,2,4,5]);
-const teamp3 = (100).toString()
-
-console.log(item,teamp,teamp2,teamp3)
-
-let a = "5"
-let b = "10"
-
-console.log(Number(a) + Number(b))
+var item = 10;
+var teamp = String(10);
+var teamp2 = String([1, 2, 4, 5]);
+var teamp3 = (100).toString();
+console.log(item, teamp, teamp2, teamp3);
+var a = "5";
+var b = "10";
+console.log(Number(a) + Number(b));
