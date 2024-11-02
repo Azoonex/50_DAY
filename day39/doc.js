164e1c12b4c5db286e1517c45b5d41794bd979b6
amirabas <amirjabic@gmail.com>
2024-11-02 23:17:04 +0330

    create random passworde

--- day39/doc.js
+++ day39/doc.js
@@ -1,42 +1,13 @@
-// const item = document.getElementById("animate")!;
-// const btn: HTMLElement | null = document.getElementById("button");
-// const demo = document.getElementById("demo")! as HTMLElement;
-// const btnStopDate = document.getElementById("stopDate")!;
-// const myInterVale = setInterval(setDate, 1000);
-// function setDate(): number | void {
-//     const date = new Date();
-//     demo.innerHTML = date.toLocaleString();
-//     console.log(date.toLocaleString());
-// }
-// btnStopDate.onclick = handleStop;
-// function handleStop() {
-//     clearInterval(myInterVale);
-// }
-// function mymove() {
-//     let id: number | null = null;
-//     let pos = 0;
-//     if (id) clearInterval(id);
-//     id = setInterval(famer, 5);
-//     function famer() {
-//         if (pos === 500) {
-//             if (id) clearInterval(id);
-//         } else if (pos === 500) {
-//             pos--;
-//             item.style.top = pos + "px";
-//             item.style.left = pos + "px";
-//         } else {
-//             pos++;
-//             item.style.top = pos + "px";
-//             item.style.left = pos + "px";
-//         }
-//     }
-// }
-// if (btn) btn.onclick = mymove;
-var item = 10;
-var teamp = String(10);
-var teamp2 = String([1, 2, 4, 5]);
-var teamp3 = (100).toString();
-console.log(item, teamp, teamp2, teamp3);
-var a = "5";
-var b = "10";
-console.log(Number(a) + Number(b));
+var d = document.getElementById("demo");
+function genrateRandomId() {
+    var pass = "";
+    var star = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < 8; i++) {
+        var char = Math.floor(Math.random() * star.length); //  create the 8 number Random 22/33/212/3/23/2
+        console.log(char);
+        pass += star.at(char);
+    }
+    return pass;
+}
+console.log(genrateRandomId());
+// let pass = ' . star value for create new Randompass 8 repete the active search
--- day39/doc.ts
+++ day39/doc.ts
@@ -1,53 +1,21 @@
-// const item = document.getElementById("animate")!;
-// const btn: HTMLElement | null = document.getElementById("button");
-// const demo = document.getElementById("demo")! as HTMLElement;
-// const btnStopDate = document.getElementById("stopDate")!;
+const d = document.getElementById("demo")!;
 
-// const myInterVale = setInterval(setDate, 1000);
+function genrateRandomId() {
+    let pass = "";
+    let star =
+        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-// function setDate(): number | void {
-//     const date = new Date();
-//     demo.innerHTML = date.toLocaleString();
-//     console.log(date.toLocaleString());
-// }
+        for(let i = 0;i < 8;i++){
+            let char = Math.floor(Math.random() * star.length); //  create the 8 number Random 22/33/212/3/23/2
+            console.log(char);
+            pass += star.at(char)
+        }
 
-// btnStopDate.onclick = handleStop;
+        return pass
 
-// function handleStop() {
-//     clearInterval(myInterVale);
-// }
+}
 
-// function mymove() {
-//     let id: number | null = null;
-//     let pos = 0;
-//     if (id) clearInterval(id);
-//     id = setInterval(famer, 5);
+console.log(genrateRandomId())
 
-//     function famer() {
-//         if (pos === 500) {
-//             if (id) clearInterval(id);
-//         } else if (pos === 500) {
-//             pos--;
-//             item.style.top = pos + "px";
-//             item.style.left = pos + "px";
-//         } else {
-//             pos++;
-//             item.style.top = pos + "px";
-//             item.style.left = pos + "px";
-//         }
-//     }
-// }
 
-// if (btn) btn.onclick = mymove;
-
-const item = 10;
-const teamp = String(10)
-const teamp2 = String([1,2,4,5]);
-const teamp3 = (100).toString()
-
-console.log(item,teamp,teamp2,teamp3)
-
-let a = "5"
-let b = "10"
-
-console.log(Number(a) + Number(b))
+// let pass = ' . star value for create new Randompass 8 repete the active search
