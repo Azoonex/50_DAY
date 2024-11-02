6db0d08a55e160c3a52035d17be233468ce78692
amirabas <amirjabic@gmail.com>
2024-11-02 23:31:30 +0330

    create the radome hixcolor

--- day39/doc.js
+++ day39/doc.js
@@ -1,13 +1,6 @@
-var d = document.getElementById("demo");
-function genrateRandomId() {
-    var pass = "";
-    var star = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    for (var i = 0; i < 8; i++) {
-        var char = Math.floor(Math.random() * star.length); //  create the 8 number Random 22/33/212/3/23/2
-        console.log(char);
-        pass += star.at(char);
-    }
-    return pass;
+var d = document.getElementById("container");
+function RadomColorHex() {
+    var n = (Math.random() * 0xffff * 100000).toString(16);
+    return "#" + n.slice(0, 6);
 }
-console.log(genrateRandomId());
-// let pass = ' . star value for create new Randompass 8 repete the active search
+d.style.background = RadomColorHex();
--- day39/doc.ts
+++ day39/doc.ts
@@ -1,21 +1,8 @@
-const d = document.getElementById("demo")!;
+const d = document.getElementById("container")! as HTMLDivElement;
 
-function genrateRandomId() {
-    let pass = "";
-    let star =
-        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-        for(let i = 0;i < 8;i++){
-            let char = Math.floor(Math.random() * star.length); //  create the 8 number Random 22/33/212/3/23/2
-            console.log(char);
-            pass += star.at(char)
-        }
-
-        return pass
-
+function RadomColorHex() {
+    let n = (Math.random() * 0xffff * 100000).toString(16);
+    return "#" + n.slice(0, 6);
 }
 
-console.log(genrateRandomId())
-
-
-// let pass = ' . star value for create new Randompass 8 repete the active search
+d.style.background = RadomColorHex()
