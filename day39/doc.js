9daaef3750de73d7be258adcb0fa662ac4f72e72
amirabas <amirjabic@gmail.com>
2024-11-03 08:36:53 +0330

    create the simple Calculator

--- day39/doc.js
+++ day39/doc.js
@@ -1,6 +1,29 @@
-var d = document.getElementById("container");
-function RadomColorHex() {
-    var n = (Math.random() * 0xffff * 100000).toString(16);
-    return "#" + n.slice(0, 6);
+var container = document.getElementById("container");
+var btnPlural = document.getElementById("plural");
+var btnMinus = document.getElementById("minus");
+var btnMultiplaction = document.getElementById("multiplaction");
+var division = document.getElementById("divistion");
+var spanDemo = document.getElementsByTagName("span")[0];
+var valueSeconde = null;
+var valueFrist = null;
+var morVlaue = null;
+function handleControlerClick(nameButton, MathSituation) {
+    nameButton.onclick = function () {
+        if (!valueFrist || !valueSeconde) {
+            valueFrist = prompt("first value ?");
+            valueSeconde = prompt("second value ?");
+            if (valueFrist || valueSeconde) {
+                spanDemo.innerHTML = String("".concat(Number(valueFrist), "  ").concat(MathSituation, "  ").concat(Number(valueSeconde), "  ").concat(MathSituation, "  ").concat(Number(morVlaue)));
+            }
+            return;
+        }
+        else if (valueFrist || valueSeconde) {
+            morVlaue = prompt("more Amount ? ");
+            spanDemo.innerHTML = String("".concat(Number(valueFrist), "  ").concat(MathSituation, "  ").concat(Number(valueSeconde), "  ").concat(MathSituation, "  ").concat(Number(morVlaue)));
+        }
+    };
 }
-d.style.background = RadomColorHex();
+handleControlerClick(btnPlural, "+");
+handleControlerClick(btnMinus, "-");
+handleControlerClick(btnMultiplaction, "*");
+handleControlerClick(division, "/");
--- day39/doc.ts
+++ day39/doc.ts
@@ -1,8 +1,39 @@
-const d = document.getElementById("container")! as HTMLDivElement;
+const container = document.getElementById("container")!;
+const btnPlural = document.getElementById("plural")!;
+const btnMinus = document.getElementById("minus")!;
+const btnMultiplaction = document.getElementById("multiplaction")!;
+const division = document.getElementById("divistion")!;
+const spanDemo = document.getElementsByTagName("span")[0]!;
 
-function RadomColorHex() {
-    let n = (Math.random() * 0xffff * 100000).toString(16);
-    return "#" + n.slice(0, 6);
+let valueSeconde: string | null = null;
+let valueFrist: string | null = null;
+let morVlaue: string | null = null;
+
+function handleControlerClick(nameButton: HTMLElement, MathSituation: string) {
+    nameButton.onclick = () => {
+        if (!valueFrist || !valueSeconde) {
+            valueFrist = prompt("first value ?");
+            valueSeconde = prompt("second value ?");
+            if (valueFrist || valueSeconde) {
+                spanDemo.innerHTML = String(
+                    `${Number(valueFrist)}  ${MathSituation}  ${Number(
+                        valueSeconde
+                    )}  ${MathSituation}  ${Number(morVlaue)}`
+                );
+            }
+            return;
+        } else if (valueFrist || valueSeconde) {
+            morVlaue = prompt("more Amount ? ");
+            spanDemo.innerHTML = String(
+                `${Number(valueFrist)}  ${MathSituation}  ${Number(
+                    valueSeconde
+                )}  ${MathSituation}  ${Number(morVlaue)}`
+            );
+        }
+    };
 }
 
-d.style.background = RadomColorHex()
+handleControlerClick(btnPlural, "+");
+handleControlerClick(btnMinus, "-");
+handleControlerClick(btnMultiplaction, "*");
+handleControlerClick(division, "/");
