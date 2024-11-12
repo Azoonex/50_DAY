5d038dc43999c9fc7be959bf7153e601524235a0
amirabas <amirjabic@gmail.com>
2024-11-12 20:47:12 +0330

    solve solution

--- day39/doc.js
+++ day39/doc.js
@@ -1,29 +1,15 @@
-var container = document.getElementById("container");
-var btnPlural = document.getElementById("plural");
-var btnMinus = document.getElementById("minus");
-var btnMultiplaction = document.getElementById("multiplaction");
-var division = document.getElementById("divistion");
-var spanDemo = document.getElementsByTagName("span")[0];
-var valueSeconde = null;
-var valueFrist = null;
-var morVlaue = null;
-function handleControlerClick(nameButton, MathSituation) {
-    nameButton.onclick = function () {
-        if (!valueFrist || !valueSeconde) {
-            valueFrist = prompt("first value ?");
-            valueSeconde = prompt("second value ?");
-            if (valueFrist || valueSeconde) {
-                spanDemo.innerHTML = String("".concat(Number(valueFrist), "  ").concat(MathSituation, "  ").concat(Number(valueSeconde), "  ").concat(MathSituation, "  ").concat(Number(morVlaue)));
-            }
-            return;
-        }
-        else if (valueFrist || valueSeconde) {
-            morVlaue = prompt("more Amount ? ");
-            spanDemo.innerHTML = String("".concat(Number(valueFrist), "  ").concat(MathSituation, "  ").concat(Number(valueSeconde), "  ").concat(MathSituation, "  ").concat(Number(morVlaue)));
-        }
-    };
+// Write a JavaScript program to add two positive integers without carrying.
+// this work the 2 positive 
+function addPositive(n1, n2) {
+    var result = 0;
+    var x = 1;
+    while (n1 > 0 && n2 > 0) {
+        result += x * ((n1 + n2) % 10);
+        n1 = Math.floor(n1 / 10);
+        n2 = Math.floor(n2 / 10);
+        x *= 10;
+    }
+    return result;
 }
-handleControlerClick(btnPlural, "+");
-handleControlerClick(btnMinus, "-");
-handleControlerClick(btnMultiplaction, "*");
-handleControlerClick(division, "/");
+console.log(addPositive(113, 123));
+// solve this
---
+++ day39/doc.ts
@@ -0,0 +1,21 @@
+// Write a JavaScript program to add two positive integers without carrying.
+// this work the 2 positive 
+
+function addPositive(n1:number,n2:number){
+    let result = 0;
+    let x = 1
+
+    while(n1> 0 && n2 > 0){
+        result += x * ((n1 + n2) % 10)
+        n1 = Math.floor(n1 / 10);
+        n2 = Math.floor(n2 / 10);
+        x *= 10;
+    }
+
+    return result
+}
+
+console.log(addPositive(113,123))
+
+// solve this
+
