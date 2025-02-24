ee9ae765e9bd317f961f3633a385ae5f173ce79d
azoonex <amirjabic@gmail.com>
2025-02-24 18:01:40 +0000

    sulation 101

--- day43/doc.js
+++ day43/doc.js
@@ -5,5 +5,41 @@
 //   return entries;
 // }
 // console.log(toNumericPairs(obj));
-var value = Object.assign({}, ['z', 't', 'm']);
-console.log(value);
+// const value = Object.assign({},['z','t','m'])
+// console.log(value)
+// function findCommon(arr1:Array<number>,arr2:Array<number>) {
+//     for(arr1)
+// }
+// solution 101
+function check_uppercase_lowerCase(str) {
+    var hasOnlyLetters = !!str.match(/^[a-z]*$/i);
+    var findUpperCaseTrue = [];
+    var matches = str.match(/[A-Z]/g);
+    if (hasOnlyLetters) {
+        matches &&
+            matches.length > 0 &&
+            matches.map(function (item) {
+                for (var x = 0; x < item.length; x++) {
+                    var findStartLetter = str.indexOf(item) + 1;
+                    var findEndLetter = str.indexOf(item) - 1;
+                    if (isLowerCase(str.split("")[findStartLetter]) &&
+                        isLowerCase(str.split("")[findEndLetter])) {
+                        findUpperCaseTrue.push(true);
+                    }
+                    else {
+                        findUpperCaseTrue.push(false);
+                    }
+                }
+            });
+    }
+    else {
+        throw new Error("Checked the text latin format");
+    }
+    return findUpperCaseTrue;
+}
+console.log(check_uppercase_lowerCase("avJaScript"));
+function isLowerCase(str) {
+    if (str) {
+        return str === str.toLowerCase() && str !== str.toUpperCase();
+    }
+}
--- day43/doc.ts
+++ day43/doc.ts
@@ -10,6 +10,45 @@
 // const value = Object.assign({},['z','t','m'])
 // console.log(value)
 
-function findCommon(arr1:Array<number>,arr2:Array<number>) {
-    for(arr1)
+// function findCommon(arr1:Array<number>,arr2:Array<number>) {
+//     for(arr1)
+// }
+
+// solution 101
+
+function check_uppercase_lowerCase(str: string) {
+  const hasOnlyLetters = !!str.match(/^[a-z]*$/i);
+  let findUpperCaseTrue: Array<boolean> = [];
+  let matches = str.match(/[A-Z]/g);
+
+  if (hasOnlyLetters) {
+    matches &&
+      matches.length > 0 &&
+      matches.map((item) => {
+        for (let x = 0; x < item.length; x++) {
+          let findStartLetter = str.indexOf(item) + 1;
+          let findEndLetter = str.indexOf(item) - 1;
+          if (
+            isLowerCase(str.split("")[findStartLetter]) &&
+            isLowerCase(str.split("")[findEndLetter])
+          ) {
+            findUpperCaseTrue.push(true);
+          } else {
+            findUpperCaseTrue.push(false);
+          }
+        }
+      });
+  } else {
+    throw new Error("Checked the text latin format");
+  }
+
+  return findUpperCaseTrue
+}
+
+console.log(check_uppercase_lowerCase("avJaScript"));
+
+function isLowerCase(str: string) {
+  if (str) {
+    return str === str.toLowerCase() && str !== str.toUpperCase();
+  }
 }
