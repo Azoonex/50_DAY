23dc2c0819d867a0e2fa7cd8cfb073ff0cb615a2
azoonex <amirjabic@gmail.com>
2025-03-01 07:36:02 +0000

    how work sort() method

--- day43/doc.js
+++ day43/doc.js
@@ -11,35 +11,70 @@
 //     for(arr1)
 // }
 // solution 101
-function check_uppercase_lowerCase(str) {
-    var hasOnlyLetters = !!str.match(/^[a-z]*$/i);
-    var findUpperCaseTrue = [];
-    var matches = str.match(/[A-Z]/g);
-    if (hasOnlyLetters) {
-        matches &&
-            matches.length > 0 &&
-            matches.map(function (item) {
-                for (var x = 0; x < item.length; x++) {
-                    var findStartLetter = str.indexOf(item) + 1;
-                    var findEndLetter = str.indexOf(item) - 1;
-                    if (isLowerCase(str.split("")[findStartLetter]) &&
-                        isLowerCase(str.split("")[findEndLetter])) {
-                        findUpperCaseTrue.push(true);
-                    }
-                    else {
-                        findUpperCaseTrue.push(false);
-                    }
-                }
-            });
-    }
-    else {
-        throw new Error("Checked the text latin format");
-    }
-    return findUpperCaseTrue;
+// function check_uppercase_lowerCase(str: string) {
+//   const hasOnlyLetters = !!str.match(/^[a-z]*$/i);
+//   let findUpperCaseTrue: Array<boolean> = [];
+//   let matches = str.match(/[A-Z]/g);
+//   if (hasOnlyLetters) {
+//     matches &&
+//       matches.length > 0 &&
+//       matches.map((item) => {
+//         for (let x = 0; x < item.length; x++) {
+//           let findStartLetter = str.indexOf(item) + 1;
+//           let findEndLetter = str.indexOf(item) - 1;
+//           if (
+//             isLowerCase(str.split("")[findStartLetter]) &&
+//             isLowerCase(str.split("")[findEndLetter])
+//           ) {
+//             findUpperCaseTrue.push(true);
+//           } else {
+//             findUpperCaseTrue.push(false);
+//           }
+//         }
+//       });
+//   } else {
+//     throw new Error("Checked the text latin format");
+//   }
+//   return findUpperCaseTrue
+// }
+// console.log(check_uppercase_lowerCase("avJaScript"));
+// function isLowerCase(str: string) {
+//   if (str) {
+//     return str === str.toLowerCase() && str !== str.toUpperCase();
+//   }
+// }
+// solution 1003
+// all method
+// const all_array = [0, 1, 2, 3, 4, 5];
+// function messed_up(arr: Array<number>) {
+//   let first_value = arr[0];
+//   let messed = arr.sort((acc, ic) => Math.random() - 5.0);
+//   let finalArray = [first_value, ...messed];
+//   return finalArray.filter((item, index) => finalArray.indexOf(item) === index);
+// }
+// console.log(messed_up(all_array));
+var numbers = [4, 2, 9, 1, 5, 7, 10, 17, 12];
+// console.log(
+//   numbers.sort((a, b) => {
+//     console.log(a, b);
+//     return b - a;
+//   })
+// );
+// let names = ["Ali", "Reza", "Zahra", "Mohammad"];
+// names.sort()
+// console.log(names)
+function findDigitNumber(arr) {
+    return arr.sort(function (a, b) {
+        if (a % 2 === 0 && b % 2 !== 0)
+            return -1;
+        if (a % 2 !== 0 && b % 2 === 0)
+            return 1;
+        if (a % 2 === 0 && b % 2 === 0)
+            return a - b;
+        if (a % 2 !== 0 && b % 2 !== 0)
+            return b - a;
+        return 0;
+    });
 }
-console.log(check_uppercase_lowerCase("avJaScript"));
-function isLowerCase(str) {
-    if (str) {
-        return str === str.toLowerCase() && str !== str.toUpperCase();
-    }
-}
+var numbers = [3, 1, 2, 8, 5, 4, 7, 6];
+console.log(findDigitNumber(numbers));
