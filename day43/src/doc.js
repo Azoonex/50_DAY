56082dc6e9fb924fea3593c54aa0d8bf67aa8fc8
azoonex <amirjabic@gmail.com>
2025-03-02 07:14:05 -0800

    add types

--- day43/src/doc.js
+++ day43/src/doc.js
@@ -1,4 +1,26 @@
 var value = [1, 2, 3, 41, 2, 14, 10];
-var newValue = value.map(function (i) { return (i); });
+var newValue = value.map(function (i) { return i; });
 var circle = 1 /* Size.sm */;
 console.log(circle);
+var action = {
+    drag: function () { },
+    resize: function () { },
+};
+// nullish coalescing operator
+var speed = null;
+var ride = {
+    ride: speed !== null && speed !== void 0 ? speed : 30,
+};
+function checkNumber(link) {
+    if (link !== undefined)
+        return 33;
+    return 0;
+}
+var input = document.getElementById("input");
+// unknown type unknown unknown unknown unknown unknown types
+function checkDocument(document) {
+    if (typeof document === "string") {
+        return document.length;
+    }
+    return "Hold";
+}
--- day43/src/doc.ts
+++ day43/src/doc.ts
@@ -1,10 +1,51 @@
-const value = [1,2,3,41,2,14,10];
+const value = [1, 2, 3, 41, 2, 14, 10];
 
-let newValue = value.map(i => (i))
+let newValue = value.map((i) => i);
 
+const enum Size {
+  sm = 1,
+  lg = 2,
+}
 
-const enum Size {sm=1,lg=2}
+const circle: Size = Size.sm;
 
-const circle:Size = Size.sm;
+console.log(circle);
 
-console.log(circle)
+// union types / aliases type / intersection type / nullable type
+
+type drag = {
+  drag: () => void;
+};
+
+type resize = {
+  resize: () => void;
+};
+
+const action: drag & resize = {
+  drag: () => {},
+  resize: () => {},
+};
+
+// nullish coalescing operator
+
+let speed: number | null = null;
+
+let ride = {
+  ride: speed ?? 30,
+};
+
+function checkNumber(link: number | string | null): number {
+  if (link !== undefined) return 33;
+  return 0;
+}
+
+const input = <HTMLInputElement>document.getElementById("input");
+
+// unknown type unknown unknown unknown unknown unknown types
+
+function checkDocument(document: unknown) {
+  if (typeof document === "string") {
+    return document.length;
+  }
+  return "Hold";
+}
