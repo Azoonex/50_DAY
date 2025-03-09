9557801578b5cbb0ca6fc82b47ca467a60c98091
azoonex <amirjabic@gmail.com>
2025-03-09 21:47:17 +0000

    Dot Product of Two 3D Vectors

--- day43/src/doc.js
+++ day43/src/doc.js
@@ -3,10 +3,29 @@ function lenghtSortArray(array) {
     var sortDigit = [];
     for (var x = 0; array.length > x; x++) {
         if (array[x + 1]) {
-            var sort = [array[array[0], x + 1]];
+            var sort = [array[(array[0], x + 1)]];
             sortDigit.push(sort);
         }
     }
     return sortDigit.length;
 }
 console.log(lenghtSortArray([1, 2, 4, 5, 7, 81]));
+function twoGiven(arr1, arr2) {
+    var vectors = [0];
+    // for(let value of arr1 ){
+    //     vectors.push(arr1[])
+    // }
+    arr1.reduce(function (_, current, index) {
+        if (current && index) {
+            vectors.push(arr1[index] + arr2[index]);
+        }
+    });
+    var sumArray = 0;
+    if (vectors.length) {
+        sumArray = vectors.reduce(function (a, s) {
+            return a += s;
+        }, 0);
+    }
+    console.log(sumArray);
+}
+twoGiven([1, 3, 14, 1, 4], [2, 1, 41, 4, 1]);
--- day43/src/doc.ts
+++ day43/src/doc.ts
@@ -1,17 +1,43 @@
 // function sort array for digit
 
+function lenghtSortArray(array: Array<number>) {
+  let sortDigit: Array<Array<number>> = [];
 
-function lenghtSortArray(array:Array<number>){
-    let sortDigit:Array<Array<number>> = [];
-
-    for(let x =0;array.length > x; x++){
-       if(array[x + 1]){
-        let sort = [array[array[0],x + 1]]
-        sortDigit.push(sort)
-       }
+  for (let x = 0; array.length > x; x++) {
+    if (array[x + 1]) {
+      let sort = [array[(array[0], x + 1)]];
+      sortDigit.push(sort);
     }
-    return sortDigit.length
+  }
+  return sortDigit.length;
 }
 
-console.log(lenghtSortArray([1,2,4,5,7,81]))
+console.log(lenghtSortArray([1, 2, 4, 5, 7, 81]));
 
+// Dot Product of Two 3D Vectors
+type number_type = number[];
+
+function twoGiven(arr1: number_type, arr2: number_type) {
+  let vectors: number_type = [0];
+  // for(let value of arr1 ){
+  //     vectors.push(arr1[])
+  // }
+  arr1.reduce((_, current, index) => {
+    if (current && index) {
+      vectors.push(arr1[index] + arr2[index]);
+    }
+  });
+
+  let sumArray = 0;
+  if (vectors.length) {
+    sumArray = vectors.reduce(function(a, s) {
+        return a += s;
+    }, 0);
+  }
+
+
+  console.log(sumArray)
+
+}
+
+twoGiven([1, 3, 14, 1, 4], [2, 1, 41, 4, 1]);
