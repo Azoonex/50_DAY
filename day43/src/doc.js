44c61187f96f8e7a971a50739773845fea071ee7
azoonex <amirjabic@gmail.com>
2025-03-19 23:31:55 +0000

    new Object keys

--- day43/src/doc.js
+++ day43/src/doc.js
@@ -1,23 +1,9 @@
-// let object = {test :1}
-// console.log(object["test"])
-function findUniqueNumber(arr) {
-    var count = arr.reduce(function (acc, num) {
-        acc[num] = (acc[num] || 0) + 1;
-        return acc;
-    }, {});
-    return Object.keys(count).find(function (key) { return count[key] === 1; });
-}
-var numbers = [5, 5, 10];
-console.log(findUniqueNumber(numbers));
-var students = ["Ali", "Reza", "Sara", "Ali", "Reza", "Ali"];
-function repeat_Name(arr) {
-    var all_student = null;
-    if (arr.length > 0) {
-        all_student = arr.reduce(function (acc, current) {
-            acc[current] = (acc[current] || 0) + 1;
-            return acc;
-        }, {});
+var params = {
+    "params": {
+        "productType": "ACCESSORY",
+        "category": "کامپیوتر",
+        "brand": "اپل",
+        "sort": "گران‌ترین"
     }
-    return all_student;
-}
-console.log(repeat_Name(students));
+};
+console.log(Object.keys(params.params));
--- day43/src/doc.ts
+++ day43/src/doc.ts
@@ -1,29 +1,12 @@
-// let object = {test :1}
-
-// console.log(object["test"])
-
-function findUniqueNumber(arr: Array<number>) {
-  const count = arr.reduce((acc: Record<number, number>, num: number) => {
-    acc[num] = (acc[num] || 0) + 1;
-    return acc;
-  }, {});
-
-  return Object.keys(count).find((key) => count[key] === 1);
-}
-
-const numbers = [5, 5, 10];
-console.log(findUniqueNumber(numbers));
-const students = ["Ali", "Reza", "Sara", "Ali", "Reza", "Ali"];
-
-function repeat_Name(arr: Array<string>) {
-  let all_student: null | Record<string, number> = null;
-  if (arr.length > 0) {
-    all_student = arr.reduce((acc: Record<string, number>, current) => {
-      acc[current] = (acc[current] || 0) + 1;
-      return acc;
-    }, {});
+let params = {
+  "params": {
+    "productType": "ACCESSORY",
+    "category": "کامپیوتر",
+    "brand": "اپل",
+    "sort": "گران‌ترین"
   }
-  return all_student;
 }
+console.log(Object.keys(params.params))
 
-console.log(repeat_Name(students));
+
+
