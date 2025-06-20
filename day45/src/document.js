a5315af34b5eac9db1f000d660b489e548a840fa
azoonex <amirjabic@gmail.com>
2025-06-20 12:55:39 +0330

    Write example function Recursion

--- day45/src/document.js
+++ day45/src/document.js
@@ -1,29 +1,10 @@
-var _a;
-var Person = /** @class */ (function () {
-    function Person(key, value) {
-        this.key = key;
-        this.value = value;
+function countDown(num) {
+    if (num <= 0) {
+        console.log("All done!");
+        return;
     }
-    Person.ArrayUtils = function (value) {
-        return [value];
-    };
-    return Person;
-}());
-var newPerson = new Person("quantity", 22);
-function fetching(url) {
-    if (url == "all") {
-        return { data: null, error: null };
-    }
-    return { data: null, error: null };
+    console.log(num);
+    num--;
+    countDown(num);
 }
-var allData = fetching("url");
-(_a = allData.data) === null || _a === void 0 ? void 0 : _a.title;
-// limit generic type
-function create(size) {
-    return size;
-}
-var createCircle = create("32");
-// lesson 63 inherit class
-// exercise 63
-// keyof
-// exercise 64
+countDown(10);
--- day45/src/document.ts
+++ day45/src/document.ts
@@ -1,53 +1,13 @@
-class Person<T, K> {
-  constructor(public key: T, private value: K) {}
-  static ArrayUtils<T>(value: T) {
-    return [value];
+function countDown(num: number) {
+  if (num <= 0) {
+    console.log("All done!");
+    return;
   }
+  console.log(num);
+
+  num--;
+  countDown(num);
 }
 
-const newPerson = new Person<string, number>("quantity", 22);
 
-// generic function interface
-
-interface Result<T> {
-  data: T | null;
-  error: string | null;
-}
-
-function fetching<T>(url: string): Result<T> {
-  if (url == "all") {
-    return { data: null, error: null };
-  }
-  return { data: null, error: null };
-}
-
-interface Products {
-  title: string;
-}
-
-interface Category {
-  name: string;
-}
-
-const allData = fetching<Products>("url");
-
-allData.data?.title;
-
-// limit generic type
-
-function create<T extends string | boolean>(size: T) {
-  return size;
-}
-
-const createCircle = create("32");
-
-// lesson 63 inherit class
-
-// exercise 63
-
-// keyof
-
-// exercise 64
-
-
-
+countDown(10)
