224778868b4be867a38c477881b519ed8cc2ad67
azoonex <amirjabic@gmail.com>
2025-06-21 00:38:04 +0330

    how work Recursion

--- day45/src/document.js
+++ day45/src/document.js
@@ -7,4 +7,23 @@ function countDown(num) {
     num--;
     countDown(num);
 }
-countDown(10);
+function sumUpTo(n) {
+    if (n === 1)
+        return 1;
+    // console.log(n + sumUpTo(n - 1))
+    return n + sumUpTo(n - 1);
+}
+console.log(sumUpTo(3));
+function reverseString(str) {
+    var method = [];
+    // method one
+    var stringToArray = str.split("");
+    method[0] = stringToArray.reverse();
+    // method two
+}
+function reverseString2(str) {
+    if (str == "")
+        return "";
+    return reverseString2(str.substr(1)) + str.charAt(0);
+}
+reverseString2("str");
--- day45/src/document.ts
+++ day45/src/document.ts
@@ -9,5 +9,32 @@ function countDown(num: number) {
   countDown(num);
 }
 
+function sumUpTo(n: number) {
+  if (n === 1) return 1;
 
-countDown(10)
+  // console.log(n + sumUpTo(n - 1))
+
+  return n + sumUpTo(n - 1);
+}
+
+console.log(sumUpTo(3));
+
+function reverseString(str: string) {
+  let method = [];
+
+  // method one
+
+  const stringToArray = str.split("");
+
+  method[0] = stringToArray.reverse();
+
+  // method two
+}
+
+function reverseString2(str: string):any {
+  if (str == "") return "";
+
+  return reverseString2(str.substr(1)) + str.charAt(0);
+}
+
+reverseString2("str")
