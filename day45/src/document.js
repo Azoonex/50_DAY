126b79d393a3cc2481aefba11a6dfad2cad9ea7f
azoonex <amirjabic@gmail.com>
2025-07-09 08:27:35 +0330

    Write logic function compact lodash

--- day45/src/document.js
+++ day45/src/document.js
@@ -1,77 +1,21 @@
-function countDown(num) {
-    if (num <= 0) {
-        console.log("All done!");
-        return;
-    }
-    console.log(num);
-    num--;
-    countDown(num);
-}
-function sumUpTo(n) {
-    if (n === 1)
-        return 1;
-    // console.log(n + sumUpTo(n - 1))
-    return n + sumUpTo(n - 1);
-}
-// console.log(sumUpTo(3));
-function reverseString(str) {
-    var method = [];
-    // method one
-    var stringToArray = str.split("");
-    method[0] = stringToArray.reverse();
-    // method two
-}
-function reverseString2(str) {
-    if (str == "")
-        return "";
-    return reverseString2(str.substr(1)) + str.charAt(0);
-}
-// reverseString2("str");
-function pageNation(size) {
-    var array = [];
-    var current_page = 3;
-    for (var x = 0; x < size; x++) {
-        var firstSize = x <= 2;
-        var centerSize = size / current_page;
-        if (firstSize) {
-            array.push(x);
+// function chunk(array: Array<number>, size: number) {
+//   let firstIndex = 0;
+//   const resultArray: Array<Array<number>> = [];
+//   for (let x = 0; x < array.length; x++) {
+//     let newArray = array.slice(firstIndex, size);
+//     (firstIndex += 2), (size += 2);
+//     if (newArray && newArray.length > 0) resultArray.push(newArray);
+//   }
+//   return resultArray;
+// }
+// console.log(chunk([1, 3, 41, 2, 9, 5, 4, 7, 8, 9], 2));
+function compact(array) {
+    var result = [];
+    array.map(function (item) {
+        if (!!item === true) {
+            result.push(item);
         }
-        else if (current_page - 3 && x == 4) {
-            array.push("...");
-        }
-    }
-    return array;
+    });
+    return result;
 }
-// console.log(pageNation(30));
-// else if (size === x + 1) {
-//       array.push(x, x + 1);
-//     }
-function fibonacci(num) {
-    if (num < 2) {
-        return num;
-    }
-    // 6 - 1 5 + 4
-    //
-    return fibonacci(num - 1) + fibonacci(num - 2);
-}
-console.log(fibonacci(6));
-function factorial(num) {
-    if (num < 2)
-        return 1;
-    return num * factorial(num - 1);
-}
-console.log(factorial(5));
-function power(base, exponent) {
-    if (exponent == 0)
-        return 1;
-    return base * power(base, exponent - 1);
-}
-<<<<<<< HEAD
-function printTree(node) {
-    console.log(node.name);
-    node.children.forEach(function (child) { return printTree(child); });
-}
-console.log(printTree());
-=======
-console.log(power(2, 3));
->>>>>>> a030fd7f09b9904cee36783dee4d6aef5362d58c
+console.log(compact([1, 2, 3, "", false, 0, 10, " ", "create"]));
--- day45/src/document.ts
+++ day45/src/document.ts
@@ -1,100 +1,25 @@
-function countDown(num: number) {
-  if (num <= 0) {
-    console.log("All done!");
-    return;
-  }
-  console.log(num);
+// function chunk(array: Array<number>, size: number) {
+//   let firstIndex = 0;
+//   const resultArray: Array<Array<number>> = [];
+//   for (let x = 0; x < array.length; x++) {
+//     let newArray = array.slice(firstIndex, size);
+//     (firstIndex += 2), (size += 2);
+//     if (newArray && newArray.length > 0) resultArray.push(newArray);
+//   }
+//   return resultArray;
+// }
 
-  num--;
-  countDown(num);
-}
+// console.log(chunk([1, 3, 41, 2, 9, 5, 4, 7, 8, 9], 2));
 
-function sumUpTo(n: number) {
-  if (n === 1) return 1;
+function compact<T>(array: Array<T>) {
+  let result: Array<T> = [];
 
-  // console.log(n + sumUpTo(n - 1))
-
-  return n + sumUpTo(n - 1);
-}
-
-// console.log(sumUpTo(3));
-
-function reverseString(str: string) {
-  let method = [];
-
-  // method one
-
-  const stringToArray = str.split("");
-
-  method[0] = stringToArray.reverse();
-
-  // method two
-}
-
-function reverseString2(str: string): any {
-  if (str == "") return "";
-
-  return reverseString2(str.substr(1)) + str.charAt(0);
-}
-// reverseString2("str");
-
-function pageNation(size: number) {
-  const array: Array<number | string> = [];
-  const current_page = 3;
-
-  for (let x = 0; x < size; x++) {
-    let firstSize = x <= 2;
-    let centerSize = size / current_page;
-    if (firstSize) {
-      array.push(x);
-    } else if (current_page - 3 && x == 4) {
-      array.push("...");
+  array.map((item) => {
+    if (!!item === true) {
+      result.push(item);
     }
-  }
-
-  return array;
+  });
+  return result;
 }
 
-// console.log(pageNation(30));
-
-// else if (size === x + 1) {
-//       array.push(x, x + 1);
-//     }
-
-function fibonacci(num: number): any {
-  if (num < 2) {
-    return num;
-  }
-
-  // 6 - 1 5 + 4
-  //
-
-  return fibonacci(num - 1) + fibonacci(num - 2);
-}
-
-console.log(fibonacci(6));
-
-function factorial(num: number): any {
-  if (num < 2) return 1;
-
-  return num * factorial(num - 1);
-}
-
-console.log(factorial(5));
-
-function power(base: number, exponent: number):any {
-  if (exponent == 0) return 1;
-
-  return base * power(base, exponent - 1);
-}
-
-function printTree(node) {
-  console.log(node.name);
-  node.children.forEach(child => printTree(child));
-}
-
-<<<<<<< HEAD
-console.log(printTree())
-=======
-console.log(power(2, 3));
->>>>>>> a030fd7f09b9904cee36783dee4d6aef5362d58c
+console.log(compact([1,2,3,"",false,0,10," ","create"]));
