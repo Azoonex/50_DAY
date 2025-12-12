88758d4c087d2c875f75844c953f46fdf2ca2409
azoonex <amirjabic@gmail.com>
2025-12-12 14:35:41 +0330

    Change name allFolder

---
+++ leetCode/app.ts
@@ -0,0 +1,27 @@
+// Valid Parentheses
+
+const input = "[{}]()";
+const correct = "[ ]( )";
+
+function isValid(s: string) {
+  const left: string[] = [];
+  const legend: Record<string, string> = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  for (const char of s) {
+    if (char in legend) {
+      console.log(char);
+      left.push(char);
+    } else {
+      const last = left.pop();
+      if (!last || legend[last] !== char) {
+        return false;
+      }
+    }
+  }
+  return !left.length;
+}
+
+console.log(isValid(input));
