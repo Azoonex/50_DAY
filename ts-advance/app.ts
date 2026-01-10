e67eab1d8851df95035047ce2c727f4ac37a78dc
azoonex <amirjabic@gmail.com>
2026-01-10 23:24:11 +0330

    Work the switch case and block scop

--- ts-advance/app.ts
+++ ts-advance/app.ts
@@ -1,12 +1,11 @@
 console.log("make item");
 
-
-function Component(constructor:Function){
-    console.log("constructor");
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertToDom = function (){
-        console.log("Insert to Dom in the Dom");
-    }
+function Component(constructor: Function) {
+  console.log("constructor");
+  constructor.prototype.uniqueId = Date.now();
+  constructor.prototype.insertToDom = function () {
+    console.log("Insert to Dom in the Dom");
+  };
 }
 
 // to way smilier
@@ -17,21 +16,51 @@ function Component(constructor:Function){
 // extends Component
 
 // Decorator factor
-function Component2(value:number){
-    return function(constructor:Function){
-        console.log("Constructor");
-        constructor.prototype.uniqueId = Date.now();
-        constructor.prototype.options = value
-    }
+function Component2(value: number) {
+  return function (constructor: Function) {
+    console.log("Constructor");
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.options = value;
+  };
 }
 
-function Pipe(constructor:Function){
-    console.log("Pipe");
-    constructor.prototype.pip = true
+function Pipe(constructor: Function) {
+  console.log("Pipe");
+  constructor.prototype.pip = true;
 }
 
-@Component2(1)
-@Pipe
-class Proformance {
+// @Component2(1)
+// @Pipe
 
+// Symbols
+
+let symbols = Symbol("11");
+console.log(symbols);
+
+const score = [50, 39, 0, 1, 32, 40];
+
+for (let i = 0; i < score.length; i++) {
+  if (score[i] === 0) {
+    console.log("I find you", score[i]);
+    continue;
+  }
+
+  //   if (score[i] === 39) {
+  //     console.log("Final score", score[i]);
+  //     break;
+  //   }
+  console.log(score[i]);
+}
+
+const grade: string = "";
+
+switch (grade) {
+  case "A":
+    console.log(100);
+    break;
+  case "B":
+    console.log("Good");
+    break;
+  default:
+    console.log("Very bad");
 }
