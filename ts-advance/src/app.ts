91b21048fe168b457d36d8f7a1db344c68b081d7
azoonex <amirjabic@gmail.com>
2026-04-27 10:06:01 +0330

    write solution with reduce

--- ts-advance/src/app.ts
+++ ts-advance/src/app.ts
@@ -1,27 +1,6 @@
-import { Factory, Setup } from "./decorator/setup";
-import { Library } from "./Library";
-import { Book } from "./module/Book";
-import { Magazine } from "./module/Magazine";
-
-const library = new Library();
-
-const b1 = new Book(1, "clean Code", "Rab s martin");
-const m2 = new Magazine(2, "nation geographic", 202);
-
-library.addItem(b1);
-library.addItem(m2);
-
-library.listAll();
-
-b1.borrow("user123");
-
-console.log(b1.isBorrowed);
-
-b1.returnItem();
-
-const setup = new Setup();
-const factory = new Factory();
-console.log(setup.createdAt);
-factory.name = "2"
-
-console.log(factory.add(2,3))
+const bil = ["age", "name"];
+const newBil = bil.reduce((acc, curr) => {
+  acc.push(curr.slice(0, 1));
+  return acc;
+}, [] as string[]);
+console.log(newBil);
